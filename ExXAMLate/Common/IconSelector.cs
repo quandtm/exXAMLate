0fc800a52949c9c3b153a387a171a6e16a75c2df
Paul <paul@jenkinsfamily.com.au>
2012-10-09 20:25:06 +1100

    Better AppX asset presentation

---
+++ ExXAMLate/Common/IconSelector.cs
@@ -0,0 +1,18 @@
+using ExXAMLate.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ExXAMLate.Common
+{
+    public class IconSelector : DataTemplateSelector
+    {
+        public DataTemplate TileTemplate { get; set; }
+        public DataTemplate IconTemplate { get; set; }
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is AppXTile)
+                return TileTemplate;
+            return IconTemplate;
+        }
+    }
+}
--- ExXAMLate/ViewModels/AppXViewModel.cs
+++ ExXAMLate/ViewModels/AppXViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.IO.Compression;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -15,8 +16,20 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace ExXAMLate.ViewModels
 {
+    public class AppXIcon
+    {
+        public BitmapImage Image { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class AppXTile : AppXIcon
+    {
+        public string AppTitle { get; set; }
+    }
     public class AppXViewModel : NotifyPropertyChangedBase
     {
+        public ObservableCollection<AppXIcon> Icons { get; set; }
         public AppXModel Model
         {
             get { return _model; }
@@ -30,9 +43,11 @@ namespace ExXAMLate.ViewModels
         private readonly INavigationManager _navigation;
         private AppXModel _model;
         public ICommand GoBack { get; set; }
+
         public AppXViewModel(INavigationManager navigation)
         {
             _navigation = navigation;
+            Icons = new ObservableCollection<AppXIcon>();
             GoBack = new DelegateCommand(() => _navigation.GoBack());
             Load();
         }
@@ -62,29 +77,29 @@ namespace ExXAMLate.ViewModels
                 var visuals = application.Element(ns + "VisualElements");
 
                 model.DisplayName = visuals.Attribute("DisplayName").Value;
+                var logo = new AppXIcon() { Description = "The \"regular\" logo is displayed as your square tile. It measures 150x150", Title = "Logo" };
                 if (visuals.Attribute("Logo") != null)
                 {
                     model.Logo = visuals.Attribute("Logo").Value;
                     var logoFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(model.DisplayName + ".png", CreationCollisionOption.ReplaceExisting);
                     await zip.GetEntry(model.Logo.Replace('\\', '/')).SaveToFile(logoFile);
-                    model.LogoImage = new BitmapImage(new Uri(logoFile.Path));
-               
-               
+                    logo.Image = new BitmapImage(new Uri(logoFile.Path));
                 }
 
+                var smallLogo = new AppXIcon { Description = "The small logo is displayed in search results, \"show all apps\", and in the search charm (vertical list). It measures 30x30.", Title = "Small Logo" };
                 if (visuals.Attribute("SmallLogo") != null)
                 {
                     var smalllogoFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(model.DisplayName + "small.png", CreationCollisionOption.ReplaceExisting);
                     await zip.GetEntry(visuals.Attribute("SmallLogo").Value.Replace('\\', '/')).SaveToFile(smalllogoFile);
-                    model.SmallLogoImage = new BitmapImage(new Uri(smalllogoFile.Path));
+                    smallLogo.Image = new BitmapImage(new Uri(smalllogoFile.Path));
                 }
 
-
+                var storeLogo = new AppXTile { Description = "The store logo is visible on the Windows Store on the web and within the Windows Store app.", Title = "Store Logo", AppTitle = model.DisplayName};
                 if (package.Element(ns + "Properties").Element(ns + "Logo") != null)
                 {
                     var storelogoimage = await ApplicationData.Current.LocalFolder.CreateFileAsync(model.DisplayName + "store.png", CreationCollisionOption.ReplaceExisting);
                     await zip.GetEntry(package.Element(ns + "Properties").Element(ns + "Logo").Value.Replace('\\', '/')).SaveToFile(storelogoimage);
-                    model.StoreLogoImage = new BitmapImage(new Uri(storelogoimage.Path));
+                    storeLogo.Image = new BitmapImage(new Uri(storelogoimage.Path));
                 }
 
 
@@ -99,6 +114,9 @@ namespace ExXAMLate.ViewModels
                 model.Background = new SolidColorBrush(ColorExtensions.FromString(visuals.Attribute("BackgroundColor").Value));
 
                 Model = model;
+                Icons.Add(smallLogo);
+                Icons.Add(storeLogo);
+                Icons.Add(logo);
             }
             catch (Exception o_0)
             {
